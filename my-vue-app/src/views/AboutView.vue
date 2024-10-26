f948bc8eb4024f115389a43a5463fb8a89b1a3b3
Piyush Wankhade <piyush.wankhade@fulcrumdigital.com>
2024-10-26 14:48:30 +0530

    Test Chnages

--- my-vue-app/src/views/AboutView.vue
+++ my-vue-app/src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
     <MainLayout>
       <h1>About Page</h1>
-      <p>This is the about page where you can learn more about us.</p>
+      <p>This is the about page where you can learn more about us as.</p>
     </MainLayout>
   </template>
   
